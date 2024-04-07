129a8c850d9952fded6136142a307cec7f6265ff
Warren Spencer <warrenspencer41@gmail.com>
2024-04-07 11:51:34 -0500

    First Commit

---
+++ src/test/java/com/warren/gate/GateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warren.gate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
