bf1036e5913d3433dd19f42e246e068144168cf0
Warren Spencer <warrenspencer41@gmail.com>
2024-04-12 06:54:28 -0500

    Adding OOP Models

--- src/main/java/com/warren/gate/model/customer.java
+++ src/main/java/com/warren/gate/model/customer.java
@@ -1,5 +1,14 @@
 package com.warren.gate.model;
 
 public class customer {
-    
+    private String name;
+    private String email;
+
+
+    public customer(String name, String email) {
+        this.name = name;
+        this.email = email;
+
+}
+
 }
