de3b2f15a3c3fd3443d9f5bd1edb79e6e4ab85fc
Warren Spencer <warrenspencer41@gmail.com>
2024-04-12 06:55:04 -0500

    Added OOP

--- src/main/java/com/warren/gate/model/customer.java
+++ src/main/java/com/warren/gate/model/customer.java
@@ -11,4 +11,24 @@ public class customer {
 
 }
 
+
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
